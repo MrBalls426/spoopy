fa5e8657aa5a036bf05a7206f4369dd1e5c2b90d
DESKTOP-CQSM1V1\Steffen <seffoom@gmail.com>
2024-06-14 16:14:01 -0700

    made the camera move away from and return to player

--- Characters/CameraPivot.gd
+++ Characters/CameraPivot.gd
@@ -13,10 +13,10 @@ const speed = 4.0
 
 var mouse_motion := Vector2.ZERO
 var FreeCamToggle: bool
+var fly: float
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-
 func _physics_process(delta: float) -> void:
 	handle_camera_rotation()
 	if not FreeCamToggle:
@@ -28,26 +28,35 @@ func handle_camera_position(penis: float) -> void:
 	global_position = lerp(global_position, character_body_3d.global_position + (Drone_position).rotated(Vector3.UP, rotation.y), penis * camera_speed)
 
 func handle_free_cam() -> void:
-	var drone_direction := Input.get_vector("fly_forward", "fly_back", "fly_left", "fly_right")
-	var flight_path := (transform.basis * Vector3(-drone_direction.y, 0, -drone_direction.x)).normalized()
-	if flight_path:
-		velocity.x = flight_path.x * speed
-		velocity.z = flight_path.z * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.z = move_toward(velocity.z, 0, speed)
-	move_and_slide()
+	if FreeCamToggle == true:
+		
+		var drone_direction := Input.get_vector("fly_forward", "fly_back", "fly_left", "fly_right")
+		var flight_path := (transform.basis * Vector3(-drone_direction.y, 0, -drone_direction.x)).normalized()
+		
+		
+		
+		if flight_path:
+			velocity.x = flight_path.x * speed
+			velocity.z = flight_path.z * speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, speed)
+			velocity.z = move_toward(velocity.z, 0, speed)
+		move_and_slide()
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 			mouse_motion = -event.relative * 0.001
+			
+		
 	if event.is_action_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		
 	if event.is_action_pressed("FreeCam"):
 		FreeCamToggle = true
-
-
-
+		
+	if event.is_action_pressed("Drone_return"):
+		FreeCamToggle = false
 func handle_camera_rotation() -> void:
 	rotate_y(mouse_motion.x)
 	camera.rotate_x(-mouse_motion.y)
--- Characters/character.gd
+++ Characters/character.gd
@@ -7,36 +7,44 @@ var mouse_motion := Vector2.ZERO
 @export var drone_position: Vector3
 
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var FreeCamToggle: bool
 
 func _ready() -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
 	handle_camera_rotation()
-	var input_dir := Input.get_vector("forward", "back", "left", "right")
-	var direction := (transform.basis * Vector3(-input_dir.y, 0, -input_dir.x)).normalized()
-	
+	if FreeCamToggle == false:
+		var input_dir := Input.get_vector("forward", "back", "left", "right")
+		var direction := (transform.basis * Vector3(-input_dir.y, 0, -input_dir.x)).normalized()
+		
 
-	if not is_on_floor():
-		if velocity.y >= 0:
-			velocity.y -= gravity * delta
+		if not is_on_floor():
+			if velocity.y >= 0:
+				velocity.y -= gravity * delta
+			else:
+				velocity.y -= gravity * delta * fall_multiplier
+
+		if direction:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
 		else:
-			velocity.y -= gravity * delta * fall_multiplier
-
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
 
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = sqrt(jump_height * 2.0 * gravity)
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = sqrt(jump_height * 2.0 * gravity)
 
-	move_and_slide()
+		move_and_slide()
 
 func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("FreeCam"):
+		FreeCamToggle = true
+		
+	if event.is_action_pressed("Drone_return"):
+		FreeCamToggle = false
+		
 	if event is InputEventMouseMotion:
 		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 			mouse_motion = -event.relative * 0.001
