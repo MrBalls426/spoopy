b849f92d9e3da9c4dc8155c965d9039f2a6828ec
Azsxa10000 <Azsxa1000@gmail.com>
2024-06-14 21:35:56 -0700

    idk what i did tbh

--- Characters/character.gd
+++ Characters/character.gd
@@ -11,45 +11,49 @@ var FreeCamToggle: bool
 
 func _ready() -> void:
 	pass
+	
 
 func _physics_process(delta: float) -> void:
 	handle_camera_rotation()
+	var input_dir : Vector2
+	var direction := (transform.basis * Vector3(-input_dir.y, 0, -input_dir.x)).normalized()
 	if FreeCamToggle == false:
-		var input_dir := Input.get_vector("forward", "back", "left", "right")
-		var direction := (transform.basis * Vector3(-input_dir.y, 0, -input_dir.x)).normalized()
+		input_dir = Input.get_vector("forward", "back", "left", "right")
+	
 		
 
-		if not is_on_floor():
-			if velocity.y >= 0:
-				velocity.y -= gravity * delta
-			else:
-				velocity.y -= gravity * delta * fall_multiplier
-
-		if direction:
-			velocity.x = direction.x * SPEED
-			velocity.z = direction.z * SPEED
+	if not is_on_floor():
+		if velocity.y >= 0:
+			velocity.y -= gravity * delta
 		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-			velocity.z = move_toward(velocity.z, 0, SPEED)
+			velocity.y -= gravity * delta * fall_multiplier
+
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 
-		if Input.is_action_just_pressed("jump") and is_on_floor():
-			velocity.y = sqrt(jump_height * 2.0 * gravity)
+	if Input.is_action_just_pressed("jump") and is_on_floor() and not FreeCamToggle:
+		velocity.y = sqrt(jump_height * 2.0 * gravity)
 
-		move_and_slide()
+	move_and_slide()
+
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("FreeCam"):
-		FreeCamToggle = true
-		
-	if event.is_action_pressed("Drone_return"):
-		FreeCamToggle = false
+		FreeCamToggle = !FreeCamToggle
+
 		
 	if event is InputEventMouseMotion:
 		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 			mouse_motion = -event.relative * 0.001
 	if event.is_action_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if Input.mouse_mode != Input.MOUSE_MODE_CAPTURED and event is InputEventMouseButton:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func handle_camera_rotation() -> void:
 	rotate_y(mouse_motion.x)
