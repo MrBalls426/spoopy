b24e4bcbecfc902469668ba51c2987a27b875e22
DESKTOP-CQSM1V1\Steffen <seffoom@gmail.com>
2024-06-15 21:23:06 -0700

    drone movement enhanced, drone flash light, change penis to delta

--- Characters/Player/Drone/Drone.gd
+++ Characters/Player/Drone/Drone.gd
@@ -15,7 +15,7 @@ var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @export_category("Nodes")
 @export var player_character: CharacterBody3D 
 @export var camera: Node3D
-
+@export var drone_light: SpotLight3D
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -25,6 +25,8 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("FreeCam"):
 		free_cam_toggle = !free_cam_toggle
 
+	if event.is_action_pressed("Drone_light"):
+		drone_light.visible = !drone_light.visible
 
 func handle_camera_rotation() -> void:
 	rotate_y(mouse_motion.x)
@@ -32,45 +34,47 @@ func handle_camera_rotation() -> void:
 	camera.rotation_degrees.x = clampf(camera.rotation_degrees.x, -90.0, 90.0)
 	mouse_motion = Vector2.ZERO
 
-
-func handle_free_cam() -> void:
+func handle_free_cam(delta: float) -> void:
 	if free_cam_toggle == true:
 		
 		var drone_direction := Input.get_vector("fly_forward", "fly_back", "fly_left", "fly_right")
 		var flight_path := (transform.basis * Vector3(-drone_direction.y, 0, -drone_direction.x)).normalized()
 		
 		if flight_path:
-			velocity.x = flight_path.x * camera_speed
-			velocity.z = flight_path.z * camera_speed
+			velocity.x = lerp(velocity.x, flight_path.x * camera_speed, delta)
+			velocity.z = lerp(velocity.z, flight_path.z * camera_speed, delta)
 		else:
-			velocity.x = move_toward(velocity.x, 0, camera_speed)
-			velocity.z = move_toward(velocity.z, 0, camera_speed)
+			velocity.x = lerp(velocity.x, 0.0, delta)
+			velocity.z = lerp(velocity.z, 0.0, delta)
 		move_and_slide()
 
-
-func handle_camera_position(penis: float) -> void:
+func handle_camera_position(delta: float) -> void:
 	#global_position = lerp(global_position, character_body_3d.global_position + (Drone_position).rotated(Vector3.UP, rotation.y), penis * camera_speed)
 	var target = player_character.global_position + (Drone_position).rotated(Vector3.UP, rotation.y)
 	var direction = global_position.direction_to(target)
 	#using move_towards for slow acceleration
-	velocity.x = lerp(velocity.x, direction.x * camera_speed, penis)
-	velocity.z = lerp(velocity.z, direction.z * camera_speed, penis)
-	velocity.y = lerp(velocity.y, direction.y * camera_speed / 2, penis)
+	velocity.x = lerp(velocity.x, direction.x * camera_speed, delta)
+	velocity.z = lerp(velocity.z, direction.z * camera_speed, delta)
+	velocity.y = lerp(velocity.y, direction.y * camera_speed / 2, delta)
 	move_and_slide()
 	## PHYSICSBODY3D AXIS LOCK LINEAR Y LATER
-
 	
 func _physics_process(delta: float) -> void:
 	handle_camera_rotation()
-	
-	if Input.is_action_pressed("fly_up") and free_cam_toggle:
-		velocity.y += gravity * delta * 2
-		
-	if Input.is_action_pressed("fly_down") and free_cam_toggle:
-		velocity.y -= gravity * delta * 2
-		
 	if free_cam_toggle:
-		handle_free_cam()
+		velocity.y += Input.get_axis("fly_down", "fly_up") * camera_speed * delta
+	
+		if Input.get_axis("fly_down", "fly_up") == 0:
+			velocity.y = lerp(velocity.y, 0.0, delta)
+	
+	#if Input.is_action_pressed("fly_up") and free_cam_toggle:
+		#velocity.y += gravity * delta * 2
+		
+	#if Input.is_action_pressed("fly_down") and free_cam_toggle:
+		#velocity.y -= gravity * delta * 2
+	
+	if free_cam_toggle:
+		handle_free_cam(delta)
 	else:
 		handle_camera_position(delta)
 
@@ -81,7 +85,5 @@ func _physics_process(delta: float) -> void:
 
 
 
-
-		
 
 
