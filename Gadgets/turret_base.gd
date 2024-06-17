bc5ca76a1eee22f85553e368faa4c27d7be187ee
DESKTOP-CQSM1V1\Steffen <seffoom@gmail.com>
2024-06-17 15:44:41 -0700

    turret stuff

---
+++ Gadgets/turret_base.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+@export var target_list : Array
+@export var max_target_distance := 200
+@export var max_target_location_range := 50
+@export var line_of_sight_required := true
+@export var within_range_required := true
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
