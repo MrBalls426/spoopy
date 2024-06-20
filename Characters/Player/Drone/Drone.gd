e74ec89a5fbd7c6814c3bf4380f21ba0868680d1
DESKTOP-CQSM1V1\Steffen <seffoom@gmail.com>
2024-06-20 15:34:06 -0700

    tried to make camera no move when first person, doesn't work

--- Characters/Player/Drone/Drone.gd
+++ Characters/Player/Drone/Drone.gd
@@ -7,44 +7,49 @@ extends CharacterBody3D
 @export var max_drone_distance := 100		## max distance in meters that the drone will work with freecam
 @export var camera_speed := 5.0				## camera move speed
 @export var camera_sensitivity := 0.002		
-@export var free_cam_toggle: bool			## toggles freecam behavior
-
+@export var recon_toggle: bool			## toggles freecam behavior
+@export var drone_return: bool
 
 @export_category("Nodes")
 @export var player_character: CharacterBody3D 
 @export var camera: Node3D
 @export var drone_light: SpotLight3D
-@onready var drone_camera: Camera3D = $DroneCamera
+@export var drone_camera: Camera3D
+
 
 
 ## USED FOR BUTTON PRESSING AND MOUSE CAMERA ROTATION
 func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("toggle_perspective"):
+		drone_return = !drone_return
 	if event is InputEventMouseMotion:
 		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-			handle_camera_rotation(event.relative * camera_sensitivity)
+			handle_drone_rotation(event.relative * camera_sensitivity)
 			
 	if event.is_action_pressed("FreeCam"):
-		free_cam_toggle = !free_cam_toggle
+		recon_toggle = !recon_toggle
 
 	if event.is_action_pressed("Drone_light"):
 		drone_light.visible = !drone_light.visible
 		
-	if event.is_action_pressed("toggle_perspective"):
+	if event.is_action_pressed("toggle_perspective") and recon_toggle:
+		!recon_toggle
 		if drone_camera.is_current():
 			drone_camera.clear_current(true)
 		else:
 			player_character.player_camera.clear_current()
 
 ## ROTATES CAMERA, ROTATION.x CLAMPED TO PREVENT CAMERA WRAPPING
-func handle_camera_rotation(mouse_motion) -> void:
-	rotate_object_local(basis.y,-mouse_motion.x)	
-	camera.rotate_object_local(camera.basis.x,mouse_motion.y)
-	camera.rotation_degrees.x = clampf(camera.rotation_degrees.x, -60.0, 40.0)
+func handle_drone_rotation(mouse_motion) -> void:
+		if !Input.is_action_just_pressed("toggle_perspective"):
+			rotate_object_local(basis.y,-mouse_motion.x)
+			camera.rotate_object_local(camera.basis.x,mouse_motion.y)
+			camera.rotation_degrees.x = clampf(camera.rotation_degrees.x, -60.0, 40.0)
 	
 
 ## HANDLES MOVEMENT FOR FREECAM
 func handle_free_cam(delta: float) -> void:
-	if free_cam_toggle == true:
+	if recon_toggle == true and not drone_return:
 		var input_direction := Input.get_vector("fly_forward", "fly_back", "fly_left", "fly_right")
 		var flight_path := (transform.basis * Vector3(-input_direction.y, 0, -input_direction.x)).normalized()
 		
@@ -65,24 +70,31 @@ func handle_free_cam(delta: float) -> void:
 
 
 ## HANDLES MOVEMENT FOR THIRD PERSON CAMERA
-func handle_camera_position(delta: float) -> void:
+func handle_drone_position(delta: float) -> void:
 	#global_position = lerp(global_position, character_body_3d.global_position + (Drone_position).rotated(Vector3.UP, rotation.y), penis * camera_speed)
 	var target = player_character.global_position + (Drone_position).rotated(Vector3.UP, rotation.y)
 	var direction = global_position.direction_to(target)
-	#using move_towards for slow acceleration
-	velocity.x = lerp(velocity.x, direction.x * camera_speed, delta)
-	velocity.z = lerp(velocity.z, direction.z * camera_speed, delta)
-	velocity.y = lerp(velocity.y, direction.y * camera_speed / 2, delta)
+	var distance = global_position.distance_to(target)
+	
+	if distance >= auto_drone_postion_range:
+		#using move_towards for slow acceleration
+		velocity.x = lerp(velocity.x, direction.x * camera_speed, delta)
+		velocity.z = lerp(velocity.z, direction.z * camera_speed, delta)
+		velocity.y = lerp(velocity.y, direction.y * camera_speed / 2.0, delta)
+	else:
+		velocity.x = lerp(velocity.x, direction.x * 0.0, delta)
+		velocity.z = lerp(velocity.z, direction.z * 0.0, delta)
+		velocity.y = lerp(velocity.y, direction.y * 0.0, delta)
 	move_and_slide()
 
 	
 	
 func _physics_process(delta: float) -> void:
 	# if in freecam  mode
-	if free_cam_toggle:
+	if recon_toggle:
 		handle_free_cam(delta)
 	else:
-		handle_camera_position(delta)
+		handle_drone_position(delta)
 
 
 
--- Characters/Player/PlayerCharacter/PlayerCharacter.gd
+++ Characters/Player/PlayerCharacter/PlayerCharacter.gd
@@ -7,31 +7,33 @@ var mouse_motion := Vector2.ZERO
 @export_category("PlayerCharacter Data")
 @export var jump_height := 1.5
 @export var fall_multiplier := 2.3
-@export var FreeCamToggle := false
+@export var ReconToggle := false
 @export var sprint_speed := 2.0
-@export var input_enabled: bool 			## Input disabled when in third person, drone locked into current state
+@export var input_enabled := true 			## Input disabled when in third person, drone locked into current state
+@export var sensitivity := 0.002
+@export var crouch_height := 0.9
+@export var height := 2.0
+@export var camera_height: float 
 
 @export_category("Nodes")
 @export var player_camera: Camera3D
+@export var mesh_instance: MeshInstance3D
+@export var collision_shape: CollisionShape3D
 
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("FreeCam"):
-		FreeCamToggle = !FreeCamToggle
+	if event.is_action_pressed("toggle_perspective"):
+		ReconToggle = !ReconToggle
 	if event is InputEventMouseMotion:
 		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-			mouse_motion = -event.relative * 0.001
+			mouse_motion = -event.relative * sensitivity
 			
 	if event.is_action_pressed("gadget"):
 		var throwgadget = Gadget.instantiate()
 		get_tree().get_first_node_in_group("gadget").add_child(throwgadget)
 		throwgadget.apply_impulse(Vector3(0.0, 5.0, 3.2).rotated(Vector3.UP, rotation.y), Vector3.ZERO)
 		throwgadget.global_position = global_position
-	
-	if event.is_action_pressed("toggle_perspective"):
-		input_enabled = !input_enabled
 
 func handle_camera_rotation() -> void:
 	rotate_y(mouse_motion.x)
@@ -41,7 +43,7 @@ func handle_camera_rotation() -> void:
 func _physics_process(delta: float) -> void:
 	handle_camera_rotation()
 	var input_dir : Vector2
-	if !FreeCamToggle and input_enabled: 
+	if !ReconToggle and input_enabled: 
 		input_dir = Input.get_vector("forward", "back", "left", "right")
 	var direction := (transform.basis * Vector3(-input_dir.y, 0, -input_dir.x)).normalized()
 		
@@ -58,15 +60,26 @@ func _physics_process(delta: float) -> void:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 			velocity.z = move_toward(velocity.z, 0, SPEED)
 
-	if Input.is_action_just_pressed("jump") and is_on_floor() and not FreeCamToggle and not input_enabled:
+	if Input.is_action_just_pressed("jump") and is_on_floor() and not ReconToggle and input_enabled:
 		velocity.y = sqrt(jump_height * 2.0 * gravity)
 		
-	if Input.is_action_pressed("sprint") and is_on_floor() and not FreeCamToggle and not input_enabled:
-		velocity.x = direction.x * SPEED * sprint_speed
-		velocity.z = direction.z * SPEED * sprint_speed
-
+	if !Input.is_action_pressed("crouch"):
+		if Input.is_action_pressed("sprint") and is_on_floor() and not ReconToggle and input_enabled:
+			velocity.x = direction.x * SPEED * sprint_speed
+			velocity.z = direction.z * SPEED * sprint_speed
+	
+	if !Input.is_action_pressed("sprint"):
+		if Input.is_action_pressed("crouch") and not ReconToggle and input_enabled:
+			mesh_instance.mesh.height = lerp(mesh_instance.mesh.height, crouch_height, delta * 2.0)
+			collision_shape.shape.height = lerp(collision_shape.shape.height, crouch_height, delta * 2.0)
+		else:
+			mesh_instance.mesh.height = lerp(mesh_instance.mesh.height, height, delta * 2.0)
+			collision_shape.shape.height = lerp(collision_shape.shape.height, height, delta * 2.0)
+		
+			player_camera.position.y = lerp(player_camera.position.y, mesh_instance.mesh.height / 2.0 - camera_height, delta * 2.0)
+	
+	
 	move_and_slide()
-
 
 
 
